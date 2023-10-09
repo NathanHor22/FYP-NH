57bf5da746f36cc87b7da10530d29197d8750bf2
Nathan Hor <nh374@sussex.ac.uk>
2023-10-09 20:34:57 +0100

    Player movement

---
+++ FYP-NH/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(CharacterController),typeof(PlayerInput))]
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float playerSpeed = 2.0f;
+    [SerializeField]
+    private float jumpHeight = 1.0f;
+    [SerializeField]
+    private float gravityValue = -9.81f;
+    [SerializeField]
+    private float rotationSpeed = .8f;
+
+    //References to playerInput component
+    private PlayerInput playerInput;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private Transform cameraTransform;
+
+    //basic player movement references
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction jumpAction;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
+        cameraTransform = Camera.main.transform;
+        moveAction = playerInput.actions["Kmove"];
+        lookAction = playerInput.actions["Klook"];
+        jumpAction = playerInput.actions["Kjump"];
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x , 0, input.y);
+
+        //Camera follows where the player is facing
+        move = move.x * cameraTransform.right.normalized + move.z * cameraTransform.forward.normalized;
+        move.y = 0f;
+
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // Changes the height position of the player..
+        if (jumpAction.triggered && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+
+        //Player looks to where the camera is directed
+        Quaternion targetRotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+}
+
