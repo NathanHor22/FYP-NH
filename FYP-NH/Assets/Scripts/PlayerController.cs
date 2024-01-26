eae3a9299c424bfc34a59d08b741fe4ef5b35e84
Nathan Hor <nathanhor2001@gmail.com>
2024-01-26 02:04:27 +0000

    pro builder stuff

--- FYP-NH/Assets/Scripts/PlayerController.cs
+++ FYP-NH/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(CharacterController),typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour
@@ -26,15 +27,32 @@ public class PlayerController : MonoBehaviour
     private InputAction moveAction;
     private InputAction lookAction;
     private InputAction jumpAction;
+    private InputAction runAction;
+    private InputAction dashAction;
 
+    //bool isRunning;
+    //float stamina = 5f;
+    //float maxStamina = 5f;
+    
+    public float dashLength = 0.5f;
+    public float dashCooldown = 1f;
+    private float dashCounter;
+    private float dashCoolCounter;
+    private float activeMoveSpeed;
+    public float dashSpeed;
+    
     private void Start()
     {
+        activeMoveSpeed = playerSpeed;
+
         controller = GetComponent<CharacterController>();
         playerInput = GetComponent<PlayerInput>();
         cameraTransform = Camera.main.transform;
         moveAction = playerInput.actions["Move"];
         lookAction = playerInput.actions["Look"];
         jumpAction = playerInput.actions["Jump"];
+        runAction = playerInput.actions["Run"];
+        dashAction = playerInput.actions["Dash"];
     }
 
     void Update()
@@ -62,10 +80,40 @@ public class PlayerController : MonoBehaviour
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
 
+        if(runAction.triggered) {
+            
+        }
+
+        if(dashAction.triggered) {
+            Debug.Log("Dashing lil bro");
+            if(dashCoolCounter <=0 && dashCounter <= 0) {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if(dashCounter > 0) {
+            dashCounter -= Time.deltaTime;
+
+            if( dashCounter <= 0) {
+                activeMoveSpeed = playerSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if(dashCoolCounter > 0) {
+            dashCoolCounter -= Time.deltaTime;
+        }
+
 
         //Player looks to where the camera is directed
         Quaternion targetRotation = Quaternion.Euler(0, cameraTransform.eulerAngles.y, 0);
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
+
+    //Concept: player can only sprint for 5 seconds 
+    //Counter to count the moment the runaction is triggered
+    //Once counter reaches 5, stop the sprint and then have cooldown on the sprint 5 seconds
+    // //after 5 seconds can sprint again
 }
 
