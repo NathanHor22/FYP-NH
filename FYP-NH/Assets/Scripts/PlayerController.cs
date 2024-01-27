51faebcb93fe7eb36e7af8ca084afd264f233870
Nathan Hor <nathanhor2001@gmail.com>
2024-01-27 02:01:30 +0000

    Animations

--- FYP-NH/Assets/Scripts/PlayerController.cs
+++ FYP-NH/Assets/Scripts/PlayerController.cs
@@ -14,7 +14,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float gravityValue = -9.81f;
     [SerializeField]
-    private float rotationSpeed = .8f;
+    private float rotationSpeed = 0.8f;
+    private float animationSmoothTime = 0.1f;
+    private float animationPlayTransition = 0.15f;
 
     //References to playerInput component
     private PlayerInput playerInput;
@@ -27,14 +29,17 @@ public class PlayerController : MonoBehaviour
     private InputAction moveAction;
     private InputAction lookAction;
     private InputAction jumpAction;
-    private InputAction runAction;
-    private InputAction dashAction;
 
-    //bool isRunning;
-    //float stamina = 5f;
-    //float maxStamina = 5f;
+    private Animator animator;
+    int jumpAnimation;
+    int moveXAnimationParameterId;
+    int moveZAnimationParameterId;
+
+    Vector2 currentAnimationBlendVector;
+    Vector2 animationVelocity;
     
-    private void Start()
+    
+    private void Awake()
     {
 
         controller = GetComponent<CharacterController>();
@@ -43,19 +48,27 @@ public class PlayerController : MonoBehaviour
         moveAction = playerInput.actions["Move"];
         lookAction = playerInput.actions["Look"];
         jumpAction = playerInput.actions["Jump"];
-        runAction = playerInput.actions["Run"];
-        dashAction = playerInput.actions["Dash"];
+        //Locks cursor to middle of the screen
+        Cursor.lockState = CursorLockMode.Locked;
+        //Animations
+        animator = GetComponent<Animator>();
+        jumpAnimation = Animator.StringToHash("Jumping");
+        moveXAnimationParameterId = Animator.StringToHash("MoveX");
+        moveZAnimationParameterId = Animator.StringToHash("MoveZ");
     }
 
     void Update()
     {
+        //Setting jumping and jump animation
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
+            
         }
         Vector2 input = moveAction.ReadValue<Vector2>();
-        Vector3 move = new Vector3(input.x , 0, input.y);
+        currentAnimationBlendVector = Vector2.SmoothDamp(currentAnimationBlendVector, input, ref animationVelocity, animationSmoothTime);
+        Vector3 move = new Vector3(currentAnimationBlendVector.x , 0, currentAnimationBlendVector.y);
 
         //Camera follows where the player is facing
         move = move.x * cameraTransform.right.normalized + move.z * cameraTransform.forward.normalized;
@@ -63,10 +76,15 @@ public class PlayerController : MonoBehaviour
 
         controller.Move(move * Time.deltaTime * playerSpeed);
 
+        //Blending the strafing animation
+        animator.SetFloat(moveXAnimationParameterId, currentAnimationBlendVector.x);
+        animator.SetFloat(moveZAnimationParameterId, currentAnimationBlendVector.y);
+
         // Changes the height position of the player..
         if (jumpAction.triggered && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            animator.CrossFade(jumpAnimation, animationPlayTransition);
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
